27aa3923766447c3abbfb463fdd3b026614bb537
Sasindu Avishka <sinharaavishka@gmail.com>
2021-12-21 18:24:22 +0530

    created the AuthFilter class

--- src/main/java/com/expenceTracker/exp_demo/repositories/repositoryImpl/UserRepositoryImpl.java
+++ src/main/java/com/expenceTracker/exp_demo/repositories/repositoryImpl/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.expenceTracker.exp_demo.repositories.repositoryImpl;
 import com.expenceTracker.exp_demo.domain.User;
 import com.expenceTracker.exp_demo.exceptions.EtAuthException;
 import com.expenceTracker.exp_demo.repositories.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -30,6 +31,9 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
+
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
+
         try {
             KeyHolder keyHolder = new GeneratedKeyHolder();
             jdbcTemplate.update(connection ->{
@@ -37,7 +41,7 @@ public class UserRepositoryImpl implements UserRepository {
                 ps.setString(1,firstName);
                 ps.setString(2,lastName);
                 ps.setString(3,email);
-                ps.setString(4,password);
+                ps.setString(4,hashedPassword);
                 return ps;
             },keyHolder);
             return (Integer) keyHolder.getKeys().get("USER_ID");
@@ -50,7 +54,7 @@ public class UserRepositoryImpl implements UserRepository {
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
         try {
             User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
-            if(!password.equals((user.getPassword())))
+            if (!BCrypt.checkpw(password, user.getPassword()))
                     throw new EtAuthException("Invalid email/password");
             return user;
         }catch (EmptyResultDataAccessException e){
